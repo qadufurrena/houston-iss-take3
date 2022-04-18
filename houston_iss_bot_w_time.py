39e17ad3aac4ae1fc545e867a26ec92cbff1732a
qadufurrena <76877511+qadufurrena@users.noreply.github.com>
2022-04-18 14:49:20 -0500

    Update and rename circle_subtract_ISS.py to houston_iss_bot_w_time.py

--- houston_iss_bot_w_time.py
+++ houston_iss_bot_w_time.py
@@ -1,3 +1,4 @@
+  
 import json
 import urllib.request
 import os
@@ -22,7 +23,7 @@ text = "Look to the Skies! The ISS is over Houston"
 #latitude: 1 deg = 110.574km
 #longitude: 1 deg = 111.320*cos(latitude)km
 #radius of visibility = 2316.4km, when=> 40deg in sky, radius = 1774.5km  
-#deploy for godsake!!!
+
 
 while True:
     url = 'http://api.open-notify.org/iss-now.json'
@@ -39,15 +40,29 @@ while True:
     Rlat = abs((center_lat-y) * 110.574)
     Rlon = abs((center_lon-x) * (111.320 * math.cos(y*0.01745329)))
     C = (math.sqrt(((Rlat) ** 2) + ((Rlon) ** 2)))
-    if C <= R:
-        print("It's here!", "lat:", lat, "lon:", lon, "Rlat:", Rlat, "Rlon:", Rlon, "C:", C)
-        api.update_status(text)
-        sleep(1800)
-    else:
-        print("nope", "lat:", lat, "lon:", lon, "Rlat:", Rlat, "Rlon:", Rlon, "C:", C)
+
+
+while True:
+    now_hour = int(datetime.now().strftime('%H'))
+    
+    if now_hour >= 20 and now_hour <= 23:
+        (lat, lon) = current_position('http://api.open-notify.org/iss-now.json')
+
+        Rlat = abs((center_lat-lat) * 110.574)
+        Rlon = abs((center_lon-lon) * (111.320 * math.cos(lat*0.01745329)))
+        C = (math.sqrt(((Rlat) ** 2) + ((Rlon) ** 2)))
+        
+        if C <= R:
+            print("It's here!", "lat:", lat, "lon:", lon, "Rlat:", Rlat, "Rlon:", Rlon, "C:", C)
+            api.update_status(text)
+            sleep(1800)
+        else:
+            print("nope", "lat:", lat, "lon:", lon, "Rlat:", Rlat, "Rlon:", Rlon, "C:", C)
+            sleep(1)
+    else: 
+        print('not here')
         sleep(5)
     
-
 
 
 
